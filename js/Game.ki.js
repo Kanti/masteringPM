4106e2164f3316eb8e7784af004c938b07a987bb
Lennart Gustafsson <l.gustafsson@web.de>
2015-07-07 15:32:09 +0200

    Corrected old Game.ki.js.

--- js/Game.ki.js
+++ js/Game.ki.js
@@ -7,47 +7,56 @@ Game.ki = {
 	botEnabled : "false",
 	botDifficulty : "none",
 	botColorId : -1,
-
-
+	
+	
+		selectDifficulty: function (botName){
+			var difficulty;
+			if(botName=="Bot-1"){
+                difficulty = 1;
+            }else if(botName=="Bot-2"){
+                difficulty = Game.static.rand(1,3);
+            }else if(botName=="Bot-3"){
+                difficulty = Game.static.rand(2,3);
+            }else if(botName=="Bot-4"){
+                difficulty = 3;
+            }else{
+                difficulty = 3;
+            }
+			//alert("F: selectDifficulty, " + difficulty);
+			return difficulty;
+		},
 		SelectField: function (botName){
 			// dort soll das nächste Feld ausgewählt werden, das angegriffen wird.
 		},
 		SelectAnswer: function (questionDifficulty,botName){
 			//dort soll die passende Antwort ausgewählt werden.
-			var rand = this.rand();
+			var rand = Game.static.rand(100);
             var botActiv = 0;
-            if(botName==1){
+            if(botName=="Bot-1"){
                 botActiv = 24;
-            }else if(botName==2){
+            }else if(botName=="Bot-2"){
                 botActiv = 48;
-            }else if(botName==3){
+            }else if(botName=="Bot-3"){
                 botActiv = 60;
-            }else if(botName==4){
+            }else if(botName=="Bot-4"){
                 botActiv = 74;
             }else{
                 botActiv = 98;
             }
-
 			if(this.questionDifficulty==1){
-                botActiv = botActiv+25; //einfach schafft es zu 49%; mittel zu 73%; schwer zu 85% hart zu 98 und extrem zu über 100%
-
+                botActiv = botActiv+25; //Bot-1 schafft es zu 49%; Bot-2 zu 73%; Bot-3 zu 85%, Bot-4 zu 98 und Bot-5 zu über 100%
 			}else if(this.questionDifficulty==2){
-				//einfach schafft es zu 24%; mittel zu 48%; schwer zu 60%...
+				//Standart Einstellungen
 			}else if(this.questionDifficulty==3){
                 botActiv = botActiv-15;
 			}
-			if(rand <= botActiv){
+			if(Game.static.rand() <= botActiv){
 				return 1; //Gibt 1 Zurück falls die Antwort richtig ist.
 			}else{
 				return 0;
 			}
 		},
-		rand: function () {
-			//Erzeugt Zufallszahlen von 1 bis Einschlieslich max, mit gleicher Wahrscheinlichkeit!
-			var min = 1, max = 100;
-			return Math.floor(Math.random() * (max - min + 1)) + min;
-		},
-        getAllFields: function($element) {
+		getAllFields: function($element) {
             var color = Game.static.getColor($element);
             var size = Game.static.getGameSize($element);
             var Fields =[];
