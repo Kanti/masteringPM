a4b6db72569d3e3f814f91c8d0c4b5fcd85a7e0a
janjj <privat@jan-jaeger.de>
2015-07-07 11:37:10 +0200

    Added 4th+5th Bot

--- js/Game.ki.js
+++ js/Game.ki.js
@@ -3,36 +3,40 @@ Game.ki = {
 	botEasyProb : 25,	//--> 25% probability
 	botModerateProb : 50,
 	botHardProb : 75,
-	botProbActiv : -1,
 	
 	botEnabled : "false",
 	botDifficulty : "none",
 	botColorId : -1,
-	
-	activ: {
-        enabled : 'false',
-        difficulty : 'none',
-		probActiv : '-1',
-		colorId :'-1'
-		},
+
+
 		SelectField: function (botName){
 			// dort soll das nächste Feld ausgewählt werden, das angegriffen wird.
 		},
 		SelectAnswer: function (questionDifficulty,botName){
 			//dort soll die passende Antwort ausgewählt werden.
 			var rand = this.rand();
-			
+            var botActiv = 0;
+            if(botName==1){
+                botActiv = 24;
+            }else if(botName==2){
+                botActiv = 48;
+            }else if(botName==3){
+                botActiv = 60;
+            }else if(botName==4){
+                botActiv = 74;
+            }else{
+                botActiv = 98;
+            }
+
 			if(this.questionDifficulty==1){
-				this.botProbActiv = this.botProbActiv+25; //einfach schafft es zu 50%; mittel zu 75%; schwer zu 100%
-				if(this.botDifficulty=="hard"){
-					this.botProbActiv = 100;
-				}
+                botActiv = botActiv+25; //einfach schafft es zu 49%; mittel zu 73%; schwer zu 85% hart zu 98 und extrem zu über 100%
+
 			}else if(this.questionDifficulty==2){
-				//einfach schafft es zu 25%; mittel zu 50%; schwer zu 75%
+				//einfach schafft es zu 24%; mittel zu 48%; schwer zu 60%...
 			}else if(this.questionDifficulty==3){
-				this.botProbActiv = this.botProbActiv-15; //einfach schafft es zu 10%; mittel zu 35%; schwer zu 60%	
+                botActiv = botActiv-15;
 			}
-			if(rand <= this.botProbActiv){
+			if(rand <= botActiv){
 				return 1; //Gibt 1 Zurück falls die Antwort richtig ist.
 			}else{
 				return 0;
